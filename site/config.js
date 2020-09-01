08fe9d673d970b6aabee112ad40e4e3e9796c3df
jalal246 <jimmy002020@gmail.com>
2020-09-01 19:12:57 +0200

    Add config file

---
+++ site/config.js
@@ -0,0 +1,31 @@
+const config = {
+  gatsby: {
+    pathPrefix: "/dflex",
+    siteUrl: "https://jalal246.github.io/dflex/",
+    trackingId: "UA-167775444-1",
+    trailingSlash: false,
+  },
+
+  siteMetadata: {
+    title: `DFlex`,
+    description: "A JavaScript ambitious project to manipulate DOM elements",
+    docsLocation: null,
+    author: `Jalal Maskoun`,
+    ogImage: null,
+    favicon: null,
+  },
+
+  header: {
+    githubUrl: "https://github.com/jalal246/dflex",
+    helpUrl: "",
+    tweetText: "",
+    social: `<li>
+		    <a href="https://twitter.com/jalal246" target="_blank" rel="noopener">
+		      <div class="twitterBtn">
+		      </div>
+		    </a>
+		  </li>`,
+  },
+};
+
+module.exports = config;
--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,69 +1,83 @@
-module.exports = {
-  pathPrefix: "/dflex",
-  siteMetadata: {
-    title: `DFlex`,
-    description: "A JavaScript ambitious project to manipulate DOM elements",
-    author: `Jalal Maskoun`,
+require("dotenv").config();
+const config = require("./config");
+
+const plugins = [
+  "gatsby-plugin-sitemap",
+  "gatsby-plugin-sharp",
+  "gatsby-plugin-emotion",
+  "gatsby-plugin-react-helmet",
+  {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: "docs",
+      path: `${__dirname}/content/`,
+    },
   },
-  plugins: [
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-emotion",
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "docs",
-        path: `${__dirname}/content/`,
+  {
+    resolve: "gatsby-plugin-mdx",
+    options: {
+      pathToConfigModule: "src/typography",
+      defaultLayouts: {
+        default: require.resolve("./src/templates/docs.js"),
       },
-    },
-    {
-      resolve: "gatsby-plugin-mdx",
-      options: {
-        pathToConfigModule: "src/typography",
-        defaultLayouts: {
-          default: require.resolve("./src/templates/docs.js"),
+      gatsbyRemarkPlugins: [
+        {
+          resolve: "gatsby-remark-images",
+          options: {
+            maxWidth: 1035,
+            sizeByPixelDensity: true,
+          },
         },
-        gatsbyRemarkPlugins: [
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              maxWidth: 1035,
-              sizeByPixelDensity: true,
-            },
-          },
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-          },
-        ],
-        extensions: [`.mdx`, `.md`],
-      },
+        {
+          resolve: "gatsby-remark-copy-linked-files",
+        },
+      ],
+      extensions: [`.mdx`, `.md`],
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `DFlex`,
-        short_name: `DFlex`,
-        start_url: `/`,
-        background_color: "#40241a",
-        theme_color: "#40241a",
-        display: `minimal-ui`,
-        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `DFlex`,
+      short_name: `DFlex`,
+      start_url: `/`,
+      background_color: "#40241a",
+      theme_color: "#40241a",
+      display: `minimal-ui`,
+      // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
     },
-    {
-      resolve: `gatsby-plugin-page-creator`,
-      options: {
-        path: `${__dirname}/src/pages`,
-      },
+  },
+  {
+    resolve: `gatsby-plugin-page-creator`,
+    options: {
+      path: `${__dirname}/src/pages`,
     },
-    {
-      resolve: `gatsby-plugin-gtag`,
-      options: {
-        trackingId: "UA-167775444-1",
-        head: true,
-        anonymize: false,
-      },
+  },
+  {
+    resolve: `gatsby-plugin-gtag`,
+    options: {
+      trackingId: config.gatsby.trackingId,
+      head: true,
+      anonymize: false,
     },
-  ],
+  },
+];
+
+module.exports = {
+  pathPrefix: config.gatsby.pathPrefix,
+  siteMetadata: {
+    title: config.siteMetadata.title,
+    description: config.siteMetadata.description,
+    docsLocation: config.siteMetadata.docsLocation,
+    ogImage: config.siteMetadata.ogImage,
+    favicon: config.siteMetadata.favicon,
+    author: config.siteMetadata.author,
+
+    githubUrl: config.header.githubUrl,
+    helpUrl: config.header.helpUrl,
+    tweetText: config.header.tweetText,
+
+    siteUrl: config.gatsby.siteUrl,
+  },
+  plugins,
 };
